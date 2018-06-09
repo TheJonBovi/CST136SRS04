99aad126161238d47cf80831f199c7daa298b9bc
Drew <35358211+TheJonBovi@users.noreply.github.com>
2018-06-08 23:36:17 -0700

    vector version almost complete
    Added function to find distances between jumps.
    Used std::accumulate to sum distances.

--- Atoll/location.h
+++ Atoll/location.h
@@ -2,6 +2,7 @@
 #include "latitude.h"
 #include "longitude.h"
 #include <string>
+#include "geo_distance.h"
 
 namespace GPS
 {
@@ -19,5 +20,12 @@ namespace GPS
 			longitude_{longitude}
 		{
 		}
+
+		static double haversine_(const GPS::Location first, const GPS::Location second)
+		{
+			return haversine_distance(first.latitude_.get_total_(), first.longitude_.get_total_(),
+										second.latitude_.get_total_(), second.longitude_.get_total_());
+		}
 	};
+
 }
